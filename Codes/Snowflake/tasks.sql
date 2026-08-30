b81fcb1f2eeb731904ee7e1ab201196503971a62
Sagar Dhavalgi <65649053+sagardhavalgi@users.noreply.github.com>
2026-08-30 20:18:13 +0530

    Update tasks.sql

--- Codes/Snowflake/tasks.sql
+++ Codes/Snowflake/tasks.sql
@@ -1,3 +1,5 @@
+-- Sample DBT models created for portfolio purposes. No client or confidential business information is included.
+
 CREATE OR REPLACE TASK TASK_CUSTOMER_CDC
 WAREHOUSE = COMPUTE_WH
 SCHEDULE = 'USING CRON 0 * * * * UTC'
