8ce17142d6f6a944c431286bbb69a6a86d75aa19
Sagar Dhavalgi <65649053+sagardhavalgi@users.noreply.github.com>
2026-08-30 20:11:33 +0530

    Create procedures.sql

---
+++ Codes/Snowflake/procedures.sql
@@ -0,0 +1,40 @@
+CREATE OR REPLACE PROCEDURE SP_LOAD_CUSTOMER()
+RETURNS STRING
+LANGUAGE SQL
+AS
+$$
+BEGIN
+
+    MERGE INTO CURATED.DIM_CUSTOMER T
+    USING (
+        SELECT *
+        FROM STR_CUSTOMER
+    ) S
+    ON T.CUSTOMER_ID = S.CUSTOMER_ID
+
+    WHEN MATCHED THEN
+        UPDATE SET
+            T.CUSTOMER_NAME = S.CUSTOMER_NAME,
+            T.EMAIL         = S.EMAIL,
+            T.UPDATED_AT    = CURRENT_TIMESTAMP()
+
+    WHEN NOT MATCHED THEN
+        INSERT
+        (
+            CUSTOMER_ID,
+            CUSTOMER_NAME,
+            EMAIL,
+            CREATED_AT
+        )
+        VALUES
+        (
+            S.CUSTOMER_ID,
+            S.CUSTOMER_NAME,
+            S.EMAIL,
+            CURRENT_TIMESTAMP()
+        );
+
+    RETURN 'CUSTOMER CDC LOAD COMPLETED';
+
+END;
+$$;
